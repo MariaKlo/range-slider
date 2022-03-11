246b914c4e2b4f69121cac7f3e2e035672960592
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-03-11 21:40:36 +0300

    Add max value to label

--- src/range-slider/Model/Model.test.ts
+++
@@ -1,4 +0,0 @@
-// import addFavoriteNumbers from'./Model';
-// test('Add two numbers', () => {
-//     expect(addFavoriteNumbers(2, 2)).toEqual(4);
-// })
--- src/range-slider/Model/Model.ts
+++ src/range-slider/Model/Model.ts
@@ -1,5 +1,11 @@
-// export default function addFavoriteNumbers(a: number, b: number) {
-//     return a + b;
-// }
+const slider = (<HTMLInputElement>document.getElementById('range-slider'));
+const maxValue: HTMLElement = document.querySelector(".label__max-value") as HTMLElement;
+const minValue: HTMLElement = document.querySelector(".label__min-value") as HTMLElement;
 
-// addFavoriteNumbers(2, 2);
+const update = () => {
+    maxValue.innerHTML = slider.value;
+    minValue.innerHTML = slider.value;
+}
+
+slider.addEventListener('click', update);
+update();
