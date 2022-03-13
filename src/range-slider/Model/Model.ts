9d79cd83b4e36993d33a6352d071356c13c5f8b9
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-03-13 22:02:17 +0300

    Creating two thumbs and labels for slider

--- src/range-slider/Model/Model.ts
+++ src/range-slider/Model/Model.ts
@@ -1,11 +1,5 @@
-const slider = (<HTMLInputElement>document.getElementById('range-slider'));
-const maxValue: HTMLElement = document.querySelector(".label__max-value") as HTMLElement;
-const minValue: HTMLElement = document.querySelector(".label__min-value") as HTMLElement;
-
-const update = () => {
-    maxValue.innerHTML = slider.value;
-    minValue.innerHTML = slider.value;
-}
-
-slider.addEventListener('click', update);
-update();
+addEventListener('input', (event: Event) => {
+    const parents = ( <HTMLElement>event.target ).parentElement;
+    const t = event.target;
+    parents.style.setProperty(`--${t.id}`, + t.value)
+}, false);
