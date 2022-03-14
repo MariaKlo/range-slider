0bbed1f663309257ef08e2d8df69c4243bfb8004
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-03-14 21:46:30 +0300

    Continue modifying two thumbs

--- src/range-slider/Model/Model.ts
+++ src/range-slider/Model/Model.ts
@@ -1,5 +1,12 @@
 addEventListener('input', (event: Event) => {
     const parents = ( <HTMLElement>event.target ).parentElement;
-    const t = event.target;
-    parents.style.setProperty(`--${t.id}`, + t.value)
+    const t = event.target as Element;
+    const inputValue = (<HTMLInputElement>document.getElementById('a')).value;
+    parents?.style.setProperty(`--${t.id}`, inputValue);
 }, false);
+
+// JavaScript version
+// addEventListener('input', e => {
+//     let t = e.target;
+//     t.parentNode.style.setProperty(`--${t.id}`, +t.value)
+//   }, false);
