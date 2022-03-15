41ca516609e056f21caa73bf71d51c94a579a93b
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-03-15 21:37:19 +0300

    Create four thumbs for slider

--- src/range-slider/Model/Model.ts
+++ src/range-slider/Model/Model.ts
@@ -1,12 +1,6 @@
+// store values in labels
 addEventListener('input', (event: Event) => {
     const parents = ( <HTMLElement>event.target ).parentElement;
-    const t = event.target as Element;
-    const inputValue = (<HTMLInputElement>document.getElementById('a')).value;
-    parents?.style.setProperty(`--${t.id}`, inputValue);
+    const t = event.target as HTMLInputElement;
+    parents?.style.setProperty(`--${t.id}`, t.value);
 }, false);
-
-// JavaScript version
-// addEventListener('input', e => {
-//     let t = e.target;
-//     t.parentNode.style.setProperty(`--${t.id}`, +t.value)
-//   }, false);
