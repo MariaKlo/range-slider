64dfff15d65430b918a38cb7419aebfad1f12ca7
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-03-22 21:19:24 +0300

    Move files to main branch

---
+++ src/range-slider/index.ts
@@ -0,0 +1,18 @@
+import './index.scss';
+// import $ from "jquery";
+// import Model from './Model/Model';
+// import {View} from './View/View';
+// import Presenter from './Presenter/Presenter';
+
+// (function($) {
+//   const view = new View()
+    /*Add here later:
+      minValue: number
+      maxValue: number
+      direction : string
+      label: boolean
+      interval: boolean
+      step: number
+    */
+  
+// })(jQuery);
