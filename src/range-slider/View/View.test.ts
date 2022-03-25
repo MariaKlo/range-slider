3245ff3abebdae625392c192e188be4aa8c66cfa
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-03-25 21:31:14 +0300

    Update test

--- src/range-slider/View/View.test.ts
+++ src/range-slider/View/View.test.ts
@@ -1,15 +1,17 @@
-import {maxDefValue} from './View';
-import $ from 'jquery';
+// import {maxDefValue} from './View';
+// import $ from 'jquery';
 
 test('clicks max and min values', () => {
-    document.body.innerHTML = 
-    '<input type="range" min="1" max="10" step="1" orient="vertical" class="range-slider">' 
-    + '<div class="label_first">' + 'Current first value: 5' +
-    '</div>' + 
-    '<div class="label__max-def-value">' + '10' + '</div>' +
-    '<div class="label__min-def-value">' + '1' + '</div>';
+    const $rangeSlider = document.querySelector(".js-range-slider") as HTMLInputElement;
+    expect($rangeSlider.value).toEqual(5);
+    // document.body.innerHTML = 
+    // '<input type="range" min="1" max="10" step="1" orient="vertical" class="range-slider">' 
+    // + '<div class="label_first">' + 'Current first value: 5' +
+    // '</div>' + 
+    // '<div class="label__max-def-value">' + '10' + '</div>' +
+    // '<div class="label__min-def-value">' + '1' + '</div>';
 
-    $('.label__max-def-value').click();
-    expect(maxDefValue).toBeCalled();
-    expect($('.label_first').text()).toEqual('Current first value: 10')
+    // $('.label__max-def-value').click();
+    // expect(maxDefValue).toBeCalled();
+    // expect($('.label_first').text()).toEqual('Current first value: 10');
 })
