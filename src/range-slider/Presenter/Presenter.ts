cd12b28c7f0d4c595a2f2beacf3bb6e2b1b3468a
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-03-25 21:32:16 +0300

    Add a draft as a comment

---
+++ src/range-slider/Presenter/Presenter.ts
@@ -0,0 +1,12 @@
+// import { View } from '../View/View';
+// import { Model } from '../Model/Model';
+
+// class Presenter {
+//   view: View;
+//   model: Model;
+//   constructor (view: View, model: Model) {
+//     this.view = view;
+//     this.model = model;
+//   }
+// }
+// export { Presenter };
