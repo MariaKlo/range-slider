dfa801420cff1738c0f75787030c5afd776049a8
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-03-29 22:18:47 +0300

    Edit view test

--- src/range-slider/View/View.test.ts
+++ src/range-slider/View/View.test.ts
@@ -1,17 +1,9 @@
-// import {maxDefValue} from './View';
-// import $ from 'jquery';
-
-test('clicks max and min values', () => {
+test('check current value of first thumb', () => {
     const $rangeSlider = document.querySelector(".js-range-slider") as HTMLInputElement;
     expect($rangeSlider.value).toEqual(5);
-    // document.body.innerHTML = 
-    // '<input type="range" min="1" max="10" step="1" orient="vertical" class="range-slider">' 
-    // + '<div class="label_first">' + 'Current first value: 5' +
-    // '</div>' + 
-    // '<div class="label__max-def-value">' + '10' + '</div>' +
-    // '<div class="label__min-def-value">' + '1' + '</div>';
+})
 
-    // $('.label__max-def-value').click();
-    // expect(maxDefValue).toBeCalled();
-    // expect($('.label_first').text()).toEqual('Current first value: 10');
+test('check current value of second thumb', () => {
+    const $secondRangeSlider = document.querySelector(".js-range-slider_second-thumb") as HTMLInputElement;
+    expect($secondRangeSlider.value).toEqual(1);
 })
