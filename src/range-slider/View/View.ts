77131e0cd5202a2b12b0fabf78001efc16d5fa33
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-03-30 22:07:19 +0300

    Edit View class

--- src/range-slider/View/View.ts
+++ src/range-slider/View/View.ts
@@ -1,3 +1,5 @@
+import { Options } from '../component/options';
+
 const $rangeSlider = document.querySelector(".js-range-slider") as HTMLInputElement;
 const $secondRangeSlider = document.querySelector(".js-range-slider_second-thumb") as HTMLInputElement;
 const $labelFirst = document.querySelector(".label_first") as HTMLDivElement;
@@ -5,95 +7,39 @@ const $labelSecond = document.querySelector(".label_second") as HTMLDivElement;
 const $maxDefValue = document.querySelector(".label__max-def-value") as HTMLDivElement;
 const $minDefValue = document.querySelector(".label__min-def-value") as HTMLDivElement;
 
-// set values of inputs
-function setValue() {
-    $rangeSlider?.addEventListener('input', () => {
-        $labelFirst.innerHTML = `Current first value: ${(<HTMLInputElement>$rangeSlider).value}`;
-    });
-}
-setValue();
 
-$secondRangeSlider?.addEventListener('input', () => {
-    $labelSecond.innerHTML = `Current second value: ${(<HTMLInputElement>$secondRangeSlider).value}`;
-});
-
-// set max and min values
-$maxDefValue.innerHTML = `Max default value: ${(<HTMLInputElement>$rangeSlider)?.max}`;
-$minDefValue.innerHTML = `Min default value: ${(<HTMLInputElement>$rangeSlider)?.min}`;
-
-// set min and max values by click on words
-function maxDefValue() {
-    $maxDefValue?.addEventListener('click', () => {
-        (<HTMLInputElement>$rangeSlider).value = (<HTMLInputElement>$rangeSlider).max;
-        $labelFirst.innerHTML = `Current value: ${(<HTMLInputElement>$rangeSlider).max}`;
-    });
-}
-maxDefValue();
-
-function minDefValue() {
-    $minDefValue?.addEventListener('click', () => {
-        (<HTMLInputElement>$rangeSlider).value = (<HTMLInputElement>$rangeSlider).min;
-        $labelFirst.innerHTML = `Current value: ${(<HTMLInputElement>$rangeSlider).min}`;
-    });
-}
-minDefValue();
-
-export {maxDefValue};
-
-// unfinished version with class View
-
-// class View {
-    // $range: <HTMLDivElement>;
-    // $wrapper:  <HTMLDivElement>;
-    // constructor($wrapper: <HTMLDivElement>, $range: <HTMLDivElement>) {
-    //     this.$wrapper = $wrapper;
-    //     this.$range = $range;
-    //     this.init();
+class View {
+    options: Options;
+    constructor(options: Options) {
+        this.options = options;
+    }
+    setMinAndMaxValues() {
+        $maxDefValue.innerHTML = `Max default value: ${this.options.max}`;
+        $minDefValue.innerHTML = `Min default value: ${this.options.min}`;
+    }
+    setFirstCurrentValue() {
+        $rangeSlider?.addEventListener('input', () => {
+            $labelFirst.innerHTML = `Current first value: ${this.options.valueFirst}`;
+        });
+    }
+    setSecondCurrentValue() {
+        $secondRangeSlider?.addEventListener('input', () => {
+            $labelSecond.innerHTML = `Current second value: ${this.options.valueSecond}`;
+        });
+    }
+    // clickOnMaxValue() {
+    //     $maxDefValue?.addEventListener('click', () => {
+    //         (<HTMLInputElement>$rangeSlider).value: number;
+    //         (<HTMLInputElement>$rangeSlider).value = this.options.max;
+    //         $labelFirst.innerHTML = `Current value: ${this.options.max}`;
+    //     });
     // }
-    // init = () => {
-    //     this.createSlider();
+    // clickOnMinValue() {
+    //     $minDefValue?.addEventListener('click', () => {
+    //         (<HTMLInputElement>$rangeSlider).value = this.options.min;
+    //         $labelFirst.innerHTML = `Current value: ${this.options.min}`;
+    //     });
     // }
-    // createSlider() {
-    //     this.$range = this.$wrapper.find('js-rage-slider');
-    //     const slider = $(`
-    //     <div class="wrap">
-    //       <input type="range" min="1" max="10" step="1" orient="vertical" class="range-slider js-range-slider" id="range-slider"/>
-    //       <div class="ticks">
-    //         <span class="tick">1</span>
-    //         <span class="tick">2</span>
-    //         <span class="tick">3</span>
-    //         <span class="tick">4</span>
-    //         <span class="tick">5</span>
-    //         <span class="tick">6</span>
-    //         <span class="tick">7</span>
-    //         <span class="tick">8</span>
-    //         <span class="tick">9</span>
-    //         <span class="tick">10</span>
-    //       </div>
-    //       <input type="range" min="1" max="10" step="1" value="1" orient="vertical" class="range-slider_second-thumb js-range-slider_second-thumb"/>
-    //       <div class="label">
-    //         <div class="label_first">Current first value: 5</div>
-    //         <div class="label_second">Current second value: 1</div>
-    //         <div class="label_max-def-value">10</div>
-    //         <div class="label_min-def-value">1</div>
-    //       </div>
-    //     </div>
-    //   `);
-    //   this.$range.append(slider);
-    // }
-//     $label: JQuery<HTMLDivElement>;
-//     $rangeSlider: JQuery<HTMLInputElement>;
-//     constructor($label: JQuery<HTMLDivElement>, $rangeSlider: JQuery<HTMLInputElement>) {
-//         this.$label = $label;
-//         this.$rangeSlider = $rangeSlider;
-//     }
-//     storeValues (): void {
-//         this.$rangeSlider.addEventListener('input', () => {
-//             this.$label.innerHTML = (<JQuery<HTMLInputElement>>this.$rangeSlider).value;
-//         });
-//     } 
-// }
+}
 
-// const view = new View($(`<div class="label">25</div>`), $(`<input type="range" min="0" max="50" step="1" class="range-slider js-range-slider" id="range-slider">`));
-// view.storeValues();
-// export { View };
+export { View };
