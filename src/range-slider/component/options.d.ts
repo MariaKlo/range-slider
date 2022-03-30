656a2c9df9db7be9d3fa9961e13ff825e8dcbab3
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-03-30 22:05:36 +0300

    Add components and edit View

---
+++ src/range-slider/component/options.d.ts
@@ -0,0 +1,43 @@
+interface Options {
+    max?: number;
+    min?: number;
+    valueFirst?: number;
+    valueSecond?: number;
+}
+
+interface SliderGlobalOptions {
+    /**
+     * Global options of the example plugin.
+     */
+    options: Options;
+}
+  
+//   /**
+//    * Function to apply the example plugin to the selected elements of a jQuery result.
+//    */
+interface SliderFunction {
+    /**
+     * Apply the example plugin to the elements selected in the jQuery result.
+     *
+     * @param options Options to use for this application of the example plugin.
+     * @returns jQuery result.
+     */
+    (options: Options): JQuery;
+}
+  
+//   /**
+//    * Declaration of the example plugin.
+//    */
+interface Slider extends SliderGlobalOptions, SliderFunction { }
+  
+//   /**
+//    * Extend the jQuery result declaration with the example plugin.
+//    */
+interface JQuery {
+    /**
+     * Extension of the example plugin.
+     */
+    examplePlugin: Slider;
+}
+
+export { Options, Slider };
--- src/range-slider/index.ts
+++ src/range-slider/index.ts
@@ -1,10 +1,15 @@
 import './index.scss';
 // import $ from "jquery";
 // import Model from './Model/Model';
-// import {View} from './View/View';
-// import Presenter from './Presenter/Presenter';
+// import { View } from './View/View';
+// // import Presenter from './Presenter/Presenter';
+// import { Options, Slider } from './component/options';
 
-
+//  // Use the plugin once the DOM has been loaded.
+//  $(function () {
+//    // Apply the plugin with additional options to override certain default options.
+//    $('#btn-alex').Slider({ outputColor: 'red' });
+//  });
 /*
 Okay you need to write it in the way that the user can write smth like this:
 
