a715fe6521e74f857ee13811a2be13fd481fadd4
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-04-04 20:00:35 +0300

    Continue fixing errors

--- src/range-slider/component/options.d.ts
+++ src/range-slider/component/options.d.ts
@@ -1,47 +1,46 @@
-import { View } from '../View/View';
-
-interface Options {
+export interface Options {
     max: number;
     min: number;
     valueFirst: number;
     valueSecond: number;
 }
 
-interface SliderGlobalOptions {
+export interface SliderGlobalOptions {
     /**
      * Global options of the example plugin.
      */
     options: Options;
 }
-  
+    
 //   /**
 //    * Function to apply the example plugin to the selected elements of a jQuery result.
 //    */
-interface SliderFunction {
-    /**
-     * Apply the example plugin to the elements selected in the jQuery result.
-     *
-     * @param options Options to use for this application of the example plugin.
-     * @returns jQuery result.
-     */
+export interface SliderFunction {
+ 
     (options: Options): JQuery;
 }
-  
+    
 //   /**
 //    * Declaration of the example plugin.
 //    */
-interface Slider extends SliderGlobalOptions, SliderFunction {
-    view: View;
+export interface Slider extends SliderGlobalOptions, SliderFunction {
+    settings: MyPluginSettings;
+    (behavior: 'enable'): JQuery;
+    (settings?: MyPluginSettings): JQuery;
 }
-  
+
+export interface MyPluginSettings {
+    title?: string;
+}
+    
 //   /**
 //    * Extend the jQuery result declaration with the example plugin.
 //    */
-interface JQuery {
-    /**
-     * Extension of the example plugin.
-     */
-    sliderPlugin: Slider;
+declare namespace global {
+    export interface JQuery {
+        /**
+         * Extension of the example plugin.
+         */
+        slider: Slider;
+    }
 }
-
-export { Options, Slider, JQuery, SliderGlobalOptions, SliderFunction };
