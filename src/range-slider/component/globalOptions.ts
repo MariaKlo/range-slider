d2a0a69e602135b16407e11a15d74dfe7d77d970
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-04-09 19:48:13 +0300

    Add new things, I don't know why I added them but okay

--- src/range-slider/component/globalOptions.ts
+++ src/range-slider/component/globalOptions.ts
@@ -3,6 +3,8 @@ export interface Options {
     min: number;
     valueFirst: number;
     valueSecond: number;
+    isVertical: boolean;
+    showBubble: boolean;
 }
 
 export interface SliderGlobalOptions {
