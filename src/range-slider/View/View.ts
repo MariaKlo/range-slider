2b434f95dbd337bfabe8337df939ea4054d0489c
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-14 22:41:07 +0300

    Edit methods, add step method

--- src/range-slider/View/View.ts
+++ src/range-slider/View/View.ts
@@ -1,9 +1,11 @@
 import Options from '../component/options';
 import {maxDefaultValue} from '../component/const';
 import {minDefaultValue}from '../component/const';
+import {defaultStep}from '../component/const';
 const values: Options = {
     max: maxDefaultValue,
     min: minDefaultValue,
+    step: defaultStep
 }
 
 const $rangeSlider = document.querySelector(".js-range-slider") as HTMLInputElement;
@@ -22,32 +24,37 @@ class View {
         this.init();
     }
     init() {
-        this.setMinAndMaxValues();
+        this.setMinAndMaxValuesForLabels();
+        this.setMaxAndMinValuesForFirstInput();
+        this.setMaxAndMinValuesForSecondInput();
         this.setFirstCurrentValue();
         this.setSecondCurrentValue();
-        this.clickOnMaxValue();
-        this.clickOnMinValue();
+        this.clickOnMaxValueForFirstInput();
+        this.clickOnMinValueForSecondInput();
     }
-    setMinAndMaxValues(): void {
+    setMinAndMaxValuesForLabels(): void {
         $maxDefValue.innerHTML = `Max default value: ${this.max}`;
         $minDefValue.innerHTML = `Min default value: ${this.min}`;
     }
+    setMaxAndMinValuesForFirstInput(): void {
+        $rangeSlider.addEventListener('input', () => {
+            const maxValue = values.max.toString();
+            $rangeSlider.max = maxValue;
+            const minValue = values.min.toString();
+            $rangeSlider.min = minValue;
+        });
+    }
+    setMaxAndMinValuesForSecondInput(): void {
+        $secondRangeSlider.addEventListener('input', () => {
+            const maxValue = values.max.toString();
+            $secondRangeSlider.max = maxValue;
+            const minValue = values.min.toString();
+            $secondRangeSlider.min = minValue;
+        });
+    }
     setFirstCurrentValue(): void {
         $rangeSlider.addEventListener('input', () => {
-            let maxValue = values.max.toString();
-            maxValue = $rangeSlider.max;
             $labelFirst.innerHTML = `Current first value: ${$rangeSlider.value}`;
-            // if (parseInt($rangeSlider.value) > this.max) {
-            //     let valueAsNumber = parseInt($rangeSlider.value);
-            //     console.log(valueAsNumber);
-            //     valueAsNumber = this.max;
-            //     $labelFirst.innerHTML = `Current first value: ${this.max}`;
-            // }
-            console.log(maxValue);
-
-            // let maxInputValueAsNumber = parseInt($rangeSlider.max);
-            // maxInputValueAsNumber = values.max;
-            // console.log(maxInputValueAsNumber);
         });
     }
     setSecondCurrentValue(): void {
@@ -55,16 +62,22 @@ class View {
             $labelSecond.innerHTML = `Current second value: ${$secondRangeSlider.value}`;
         });
     }
-    clickOnMaxValue(): void {
+    clickOnMaxValueForFirstInput(): void {
         $maxDefValue.addEventListener('click', () => {
             $rangeSlider.valueAsNumber = this.max;
             $labelFirst.innerHTML = `Current first value: ${this.max}`;
         });
     }
-    clickOnMinValue(): void {
+    clickOnMinValueForSecondInput(): void {
         $minDefValue.addEventListener('click', () => {
-            $rangeSlider.valueAsNumber = this.min;
-            $labelSecond.innerHTML = `Current first value: ${this.min}`;
+            $secondRangeSlider.valueAsNumber = this.min;
+            $labelSecond.innerHTML = `Current second value: ${this.min}`;
+        });
+    }
+    setStepForFirstInput(): void {
+        $rangeSlider.addEventListener('input', () => {
+            const steps = values.step.toString();
+            $rangeSlider.step = steps;
         });
     }
 }
