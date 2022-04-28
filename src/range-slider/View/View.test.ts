80961dfa52cab39fde073633364e1660e49b19fd
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-28 22:04:56 +0300

    Delete slider folder. Start from scratch

--- src/range-slider/View/View.test.ts
+++
@@ -1,9 +0,0 @@
-test('check current value of first thumb', () => {
-    const $rangeSlider = document.querySelector(".js-range-slider") as HTMLInputElement;
-    expect($rangeSlider.value).toEqual(5);
-})
-
-test('check current value of second thumb', () => {
-    const $secondRangeSlider = document.querySelector(".js-range-slider_second-thumb") as HTMLInputElement;
-    expect($secondRangeSlider.value).toEqual(1);
-})
--- src/range-slider/component/const.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export const maxDefaultValue = 10;
-export const minDefaultValue = 1;
-export const defaultStep = 2;
-
-// export default {
-//     maxDefaultValue,
-//     minDefaultValue
-// };
