0393c13ad132b1f17c1c42350afbe6986d48500b
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-05-01 18:36:39 +0300

    Start testing View

--- src/range-slider/Model/Model.test.ts
+++
@@ -1,6 +0,0 @@
-import { CustomOptions } from "./Model";
-
-test('find min from class', () => {
-    const custom = new CustomOptions();
-    expect(typeof custom.min).toBe("undefined");
-})
---
+++ src/range-slider/View/subView/barView/barView.ts
@@ -0,0 +1,5 @@
+class barView {
+    
+}
+
+export default barView;
