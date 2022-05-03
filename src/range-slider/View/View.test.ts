49dd38a278e3169ea0f17bc027d3f87e6d63d7f4
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-05-03 20:43:04 +0300

    Add bubble

--- src/range-slider/View/View.test.ts
+++ src/range-slider/View/View.test.ts
@@ -4,29 +4,17 @@ import View from './View';
 describe('test view', () => {
     let view: View;
 
-    const thumbOptions = {
-        setTwoThumbs: false,
-        firstValue: 10,
-        secondValue: 50,
-        percent: 30,
-        percentSecond: 60
-    };
-
     let createRangeSlider = () => {
         document.body.innerHTML = '';
-        view.init(document.body, true, -200, 100, true, 0, 70);
-    };
-
-    const createThumb = () => {
-       view.createThumb(document.body, thumbOptions.setTwoThumbs);
+        view.init(document.body, true, -200, 100);
     };
 
     beforeEach(() => {
         view = new View();
-        view.init(document.body, false, 0, 100, false, 20, 30);
+        view.init(document.body, false, 0, 100);
         createRangeSlider = () => {
             document.body.innerHTML = '';
-            view.init(document.body, true, -200, 100, true, 0, 70);
+            view.init(document.body, true, -200, 100);
         };
     });
     afterEach(() => {
@@ -62,31 +50,5 @@ describe('test view', () => {
         view.setMin(true, 0);
         expect(view.input.min).toBe('0');
         expect(view.secondInput.min).toBe('0');
-    });
-
-    // tests for thumb
-    test('value for first thumb is set correctly', () => {
-        const { setTwoThumbs, firstValue } = thumbOptions;
-        view.createThumbElement(setTwoThumbs, document.body);
-        view.setThumbValue(setTwoThumbs, firstValue);
-        expect(view.showThumb.textContent).toBe(String(firstValue));
-    });
-
-    test('value for two thumbs is set correctly', () => {
-        thumbOptions.setTwoThumbs = true;
-        const { setTwoThumbs, firstValue, secondValue } = thumbOptions;
-        view.createThumbElement(setTwoThumbs, document.body, document.body);
-        view.setThumbValue(setTwoThumbs, firstValue, secondValue);
-        expect(view.showThumb.textContent).toBe(String(firstValue));
-        expect(view.showSecondThumb.textContent).toBe(String(secondValue));
-    });
-
-    test('set two thumbs on input', () => {
-        thumbOptions.setTwoThumbs = true;
-        createThumb();
-        const { setTwoThumbs, percent, percentSecond } = thumbOptions;
-        view.setThumb(setTwoThumbs, percent, percentSecond);
-        expect(view.firstThumb.style.left).toBe(`${percent}%`);
-        expect(view.secondThumb.style.right).toBe(`${100 - percentSecond}%`);
     });
 });
