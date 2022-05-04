527870708f440ed29a46b86b967bd29c2e50755d
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-05-04 21:35:03 +0300

    Add ticks

---
+++ src/range-slider/View/subView/ticksView/ticksView.ts
@@ -0,0 +1,33 @@
+class ticksView {
+  createTicks = (ticksValues: Array<number>, width: number) => {
+    const ticksElement = document.createElement('div');
+    ticksElement.classList.add('range-slider__ticks');
+
+    const values = [];
+    for (let i = 0; i < ticksValues.length; i += 1) {
+      const divValue: HTMLDivElement = document.createElement('div');
+      divValue.classList.add('range-slider__tick');
+      // add tick
+      const value: number = ticksValues[i];
+      divValue.textContent = String(`– ${value}`);
+      ticksElement.append(divValue);
+      // set length of ticks
+      const min = ticksValues[0];
+      const max = ticksValues[ticksValues.length - 1];
+      const percent: number = Math.round(((value - min) / (max - min)) * 100);
+      divValue.style.left = `${percent}%`;
+
+      values.push({ element: divValue, value });
+
+      divValue.style.marginLeft = `-${this.placeTicks(width)}%`;
+    }
+    return {
+      ticksElement,
+      values,
+    };
+  };
+    
+  placeTicks = (containerWidth: number): number => (0.42 * containerWidth + 777.8) / containerWidth;
+}
+
+export default ticksView;
