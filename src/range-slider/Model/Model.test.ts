5f2938b11bdf723dfd691f4e0effcf0d5c5213bd
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-05-11 19:22:55 +0300

    Add model and model tests

--- src/range-slider/Model/Model.test.ts
+++ src/range-slider/Model/Model.test.ts
@@ -1,6 +1,64 @@
-import { CustomOptions } from "./Model";
+import Model from './Model';
 
-test('find min from class', () => {
-    const custom = new CustomOptions();
-    expect(typeof custom.min).toBe("undefined");
-})
+describe('test model', () => {
+    let model: Model;
+
+    beforeEach(() => {
+        model = new Model();
+    });
+
+    test('max value set to 100 if not defined', () => {
+        expect(model.max).toBe(100);
+    });
+    test('default value set to 50 if not defined', () => {
+        expect(model.defaultValue).toBe(50);
+    });
+    test('default value is set', () => {
+        const newDefaultValue = 35;
+        model.setDefaultValue(newDefaultValue);
+        expect(model.defaultValue).toBe(newDefaultValue);
+    });
+    test('second value set to 70 if not defined', () => {
+        expect(model.valueSecond).toBe(70);
+    });
+    test('second value is set', () => {
+        const newValueSecond = 123;
+        model.setSecondValue(newValueSecond);
+        expect(model.valueSecond).toBe(newValueSecond);
+    });
+    test('values for ticks recieved', () => {
+        expect(model.getTicks(0, 1, 1, false)).toEqual([]);
+        const expectArray = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+        expect(model.getTicks(0, 100, 10, true)).toEqual(expectArray);
+    });
+    test('step is overwritten when it is less or equal to 0', () => {
+        const expectArray = [1, 2, 3, 4, 5];
+        expect(model.getTicks(1, 5, 0, true)).toEqual(expectArray);
+        expect(model.getTicks(1, 5, -100, true)).toEqual(expectArray);
+    });
+    test('last value for ticks is equal to max value', () => {
+    // even if it doesn't fit the step
+    const expectArray = [0, 5, 10, 16];
+        expect(model.getTicks(0, 16, 5, true)).toEqual(expectArray);
+    });
+    test('nearest value is calculated', () => {
+        let values = {
+            newValue: 5,
+            step: 2,
+            nearestFitsStep: 4,
+        };
+        expect(model.calcNearestMaxValueConsideringStep(values.newValue, values.step)).toBe(values.nearestFitsStep);
+        values = {
+            newValue: 8,
+            step: 3,
+            nearestFitsStep: 9,
+        };
+        expect(model.calcNearestMaxValueConsideringStep(values.newValue, values.step)).toBe(values.nearestFitsStep);
+        values = {
+            newValue: -1,
+            step: 10,
+            nearestFitsStep: 0,
+        };
+        expect(model.calcNearestMaxValueConsideringStep(values.newValue, values.step)).toBe(values.nearestFitsStep);
+    });
+});
--- src/range-slider/Model/Model.ts
+++ src/range-slider/Model/Model.ts
@@ -1,5 +1,9 @@
 import Options from "../component/globalOptions";
 
+interface IObserverModel {
+    updateView(): void
+}
+
 class Model {
     optionsForView!: Options;
     max!: number;
@@ -8,24 +12,26 @@ class Model {
     defaultValue!: number;
     valueSecond!: number;
     isMultiThumb!: boolean;
-    rightProgressBar!: boolean;
+    showRightProgressBar!: boolean;
     showBubble!: boolean;
     isVertical!: boolean;
     showTicks!: boolean;
     ticksValues!: number[];
+    observers!: IObserverModel[];
 
-    constructor(options: Options) {
-        this.max = Number(options.max || 100);
-        this.min = Number(options.min || 0);
-        this.step = Number(options.step || 1);
-        this.defaultValue = Number(options.defaultValue || 50);
-        this.valueSecond = Number(options.valueSecond || 70);
-        this.isMultiThumb = options.isMultiThumb || false;
-        this.rightProgressBar = options.showRightProgressBar || false;
-        this.showBubble = options.showBubble || false;
-        this.isVertical = options.isVertical || false;
-        this.showTicks = options.showTicks || false;
+    constructor() {
+        this.max = Number(this.max || 100);
+        this.min = Number(this.min || 0);
+        this.step = Number(this.step || 1);
+        this.defaultValue = Number(this.defaultValue || 50);
+        this.valueSecond = Number(this.valueSecond || 70);
+        this.isMultiThumb = this.isMultiThumb || false;
+        this.showRightProgressBar = this.showRightProgressBar || false;
+        this.showBubble = this.showBubble || false;
+        this.isVertical = this.isVertical || false;
+        this.showTicks = this.showTicks || false;
         this.ticksValues = [];
+        this.observers = [];
 
         this.optionsForView = {
             max: this.max,
@@ -34,12 +40,115 @@ class Model {
             defaultValue: this.defaultValue,
             valueSecond: this.valueSecond,
             isMultiThumb: this.isMultiThumb,
-            showRightProgressBar: this.rightProgressBar,
+            showRightProgressBar: this.showRightProgressBar,
             showBubble: this.showBubble,
             isVertical: this.isVertical,
             showTicks: this.showTicks,
-            ticksValues: this.ticksValues
+            ticksValues: this.getTicks(),
         };
+    }
+
+    init() {
+        this.getTicks();
+    }
+
+    subscribe(observer: IObserverModel) {
+        this.observers.push(observer);
+    }
+    
+    update(newVal: number, isDefault: boolean) {
+        if (this.isMultiThumb) {
+            this.limitToggle(newVal, isDefault);
+        } else {
+            this.limitStep(newVal, isDefault);
+        }
+    }
+
+    updateObservers() {
+        this.observers.forEach((observer) => {
+            observer.updateView();
+        });
+    }
+
+    setDefaultValue(value: number) {
+        this.defaultValue = value;
+    }
+
+    setSecondValue(value: number) {
+        this.valueSecond = value;
+    }
+
+    // calculate steps and ticks
+    calcNearestMaxValueConsideringStep(newValue: number, step: number = this.step): number {
+        const roundToMin = newValue - (newValue % step);
+        if ((newValue % step) > (step / 2)) {
+            return step + roundToMin;
+        }
+        return roundToMin;
+    }
+    
+    getTicks(min: number = this.min, max: number = this.max, step: number = this.step, 
+        showTicks: boolean = this.showTicks): number[] {
+        if (step <= 0) {
+            step = 1;
+        }
+        const ticksValues: number[] = [];
+        if (!showTicks) {
+            return ticksValues;
+        }
+        const possibleValues = (max - min) / step;
+
+        if (possibleValues <= 11) {
+            for (let i: number = min; i <= max; i += step) {
+            ticksValues.push(i);
+            }
+        } else {
+        const ticksStep = Math.round(possibleValues / 10);
+        let currentValue = min;
+        for (let i = 0; i < possibleValues; i += ticksStep) {
+            if (currentValue <= max) {
+                ticksValues.push(currentValue);
+            }
+            currentValue += step * ticksStep;
+            }
+            if (currentValue <= max) {
+            ticksValues.push(currentValue);
+            }
+        }
+        if (ticksValues[ticksValues.length - 1] !== max) {
+            ticksValues.pop();
+            ticksValues.push(max);
+        }
+
+        return ticksValues;
+    }
+
+    limitToggle(newValue: number, isDefault: boolean) {
+        const isInRange = isDefault ? newValue < this.valueSecond : newValue > this.defaultValue;
+        if (isInRange) {
+          this.limitStep(newValue, isDefault);
+        } else {
+          this.updateObservers();
+        }
+    }
+    
+    limitStep(newValue: number, isDefault: boolean) {
+    const isInStep = newValue % this.step === 0;
+    if (isDefault) {
+        if (isInStep) {
+            this.setDefaultValue(newValue);
+        } else {
+            const value: number = this.calcNearestMaxValueConsideringStep(newValue);
+            this.setDefaultValue(value);
+            this.updateObservers();
+        }
+        } else if (isInStep) {
+            this.setSecondValue(newValue);
+        } else {
+            const value: number = this.calcNearestMaxValueConsideringStep(newValue);
+            this.setSecondValue(value);
+            this.updateObservers();
+        }
     }
 }
 
