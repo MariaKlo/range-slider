31be74574a9b1f7e3e884d030180da3afecbf7a8
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-05-15 19:17:59 +0300

    Experiment with jquery

--- src/range-slider/index.ts
+++ src/range-slider/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // import './jquery.index.d.ts';
-import $ from 'jquery';
+// import $ from 'jquery';
 
-$('#plugin').Slider({
-    max: 500,
-    min: 50,
-    step: 1,
-});
+// $('#plugin').Slider({
+//     max: 500,
+//     min: 50,
+//     step: 1,
+// });
