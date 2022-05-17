effc473f3511b7697118b3220ae14d4ee0036498
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-05-17 22:24:23 +0300

    Add tests

--- src/range-slider/View/subView/bubbleView/bubbleView.test.ts
+++ src/range-slider/View/subView/bubbleView/bubbleView.test.ts
@@ -26,7 +26,7 @@ describe('test view', () => {
         expect(bubble.showBubble.textContent).toBe(String(firstValue));
     });
 
-    test('value for two thumbs is set correctly', () => {
+    test('values of bubbles for two thumbs are set correctly', () => {
         bubbleOptions.setTwoThumbs = true;
         const { setTwoThumbs, firstValue, secondValue } = bubbleOptions;
         bubble.createBubbleElement(setTwoThumbs, document.body, document.body);
@@ -34,4 +34,39 @@ describe('test view', () => {
         expect(bubble.showBubble.textContent).toBe(String(firstValue));
         expect(bubble.showSecondBubble.textContent).toBe(String(secondValue));
     });
+
+    test('create bubble wrapper with double thumbs', () => {
+        jest.spyOn(bubble, 'createBubbleWrapper');
+        bubble.createBubbleWrapper(document.body, true);
+        expect(bubble.createBubbleWrapper).toHaveBeenCalledTimes(1);
+    });
+
+    // crushed tests below
+    test('two bubbles are rotated when slider is vertical', () => {
+        // const mockedElementDOM = { classList: { contains: jest.fn(), remove: jest.fn(), add: jest.fn() } };
+        jest.spyOn(bubble, 'rotateBubble');
+        // bubble.rotateBubble();
+        // const className = 'range-slider__bubble_vertical';
+        // mockedElementDOM.classList.contains.mockReturnValueOnce(false);
+        bubble.rotateBubble();
+        expect(bubble.rotateBubble).toBeCalled();
+    });
+
+    // test('set default bubble considering percent', () => {
+    //     jest.spyOn(bubble, 'setBubble');
+    //     bubble.setBubble(bubbleOptions.setTwoThumbs, bubbleOptions.percent, undefined);
+    //     expect(bubble.firstBubble.style.left).toEqual(`30%`);
+    // });
+
+    // test('set two bubbles considering percent and double thumbs', () => {
+    //     jest.spyOn(bubble, 'setBubble');
+    //     bubble.setBubble(true, bubbleOptions.percent, bubbleOptions.percentSecond);
+    //     expect(bubble.secondBubble.style.right).toEqual(`40%`);
+    // });
+
+    // test('rotate second bubble to vertical position', () => {
+    //     const classNameVertical = 'range-slider__bubble_vertical';
+    //     bubble.rotateBubble();
+    //     expect(bubble.showSecondBubble.classList).toEqual(classNameVertical);
+    // });
 });
