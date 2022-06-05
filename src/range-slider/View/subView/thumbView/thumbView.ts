addacbe22cb910915fddac4314edfdc795410b60
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-06-05 18:22:05 +0300

    Fix error

--- src/range-slider/View/subView/thumbView/thumbView.ts
+++ src/range-slider/View/subView/thumbView/thumbView.ts
@@ -3,7 +3,7 @@ class ThumbView {
   firstThumb!: HTMLElement;
   secondThumb!: HTMLElement;
   showBubble!: HTMLElement;
-  showSecondBubble?: HTMLElement;
+  showSecondBubble!: HTMLElement;
 
   init(parent: HTMLElement, isDouble: boolean, toggleElement: boolean, defaultValue: number,
       secondValue?: number) {
@@ -14,21 +14,21 @@ class ThumbView {
     } 
   }
 
-    createThumb(parent: HTMLElement, isDouble: boolean) {
-      this.firstThumb = document.createElement('div');
-      this.firstThumb.className = 'range-slider__thumb';
-      parent.append(this.firstThumb);
+  createThumb(parent: HTMLElement, isDouble: boolean) {
+    this.firstThumb = document.createElement('div');
+    this.firstThumb.className = 'range-slider__thumb';
+    parent.append(this.firstThumb);
 
-      if (isDouble) {
-        this.firstThumb.classList.add('range-slider__thumb_first');
-        this.secondThumb = document.createElement('div');
-        this.secondThumb.classList.add('range-slider__thumb');
-        this.secondThumb.classList.add('range-slider__thumb_second');
-        parent.append(this.secondThumb);
-      }
+    if (isDouble) {
+      this.firstThumb.classList.add('range-slider__thumb_first');
+      this.secondThumb = document.createElement('div');
+      this.secondThumb.classList.add('range-slider__thumb');
+      this.secondThumb.classList.add('range-slider__thumb_second');
+      parent.append(this.secondThumb);
     }
+  }
 
-    createBubbleElement(isDouble: boolean, parent: HTMLElement, secondParent?: HTMLElement): void {
+  createBubbleElement(isDouble: boolean, parent: HTMLElement, secondParent?: HTMLElement): void {
     this.showBubble = document.createElement('p');
     this.showBubble.classList.add('range-slider__bubble');
     parent.append(this.showBubble);
@@ -39,23 +39,23 @@ class ThumbView {
     } 
   }
 
-    writeBubbleValue(isMultiThumb: boolean, value: number, secondValue?: number): void {
-      if (this.showBubble) {
-        this.showBubble.textContent = `First value: ${String(value)}`;
-        if (isMultiThumb) {
-          this.showSecondBubble!.textContent = `Second value: ${String(secondValue)}`
-        }
+  writeBubbleValue(isMultiThumb: boolean, value: number, secondValue?: number): void {
+    if (this.showBubble) {
+      this.showBubble.textContent = `First value: ${String(value)}`;
+      if (isMultiThumb) {
+        this.showSecondBubble.textContent = `Second value: ${String(secondValue)}`
       }
     }
+  }
 
-    placeThumb(isDouble: boolean, percent: number, percentSecond?: number): void {
-      this.firstThumb.style.left = `${percent}%`;
-      if (isDouble) {
-        this.secondThumb.style.right = `${100 - (percentSecond || 0)}%`;
-      }
+  placeThumb(isDouble: boolean, percent: number, percentSecond?: number): void {
+    this.firstThumb.style.left = `${percent}%`;
+    if (isDouble) {
+      this.secondThumb.style.right = `${100 - (percentSecond! || 0)}%`;
     }
+  }
 
-    rotateBubble(): void {
+  rotateBubble(): void {
     const classNameVertical = 'range-slider__bubble_vertical';
     this.showBubble.classList.add(classNameVertical);
     if (this.showSecondBubble) {
