991a44b3230f10e6867cf07de3ce36315d595909
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-06-08 19:05:54 +0300

    Add file to call jquery api

---
+++ src/demo-page/jquery.callPlugin.ts
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import './jquery.index';
+
+$('#api_first').sliderPlugin({
+  max: 100,
+  min: 0,
+  step: 5,
+ defaultValue: 60,
+  valueSecond: 70,
+  isMultiThumb: false,
+  showBubble: true,
+  isVertical: false,
+  showTicks: true,
+  ticksValues: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+});
+  
+$('#api_second').sliderPlugin({
+  max: 50,
+  min: 0,
+  step: 1,
+  defaultValue: 10,
+  valueSecond: 30,
+  isMultiThumb: true,
+  showBubble: false,
+  isVertical: false,
+  showTicks: false,
+  ticksValues: [0, 10, 20, 30, 40, 50],
+});
+  
+$('#api_third').sliderPlugin({
+  max: 100,
+  min: 0,
+  step: 10,
+  defaultValue: 40,
+  valueSecond: 90,
+  isMultiThumb: true,
+  showBubble: false,
+  isVertical: true,
+  showTicks: false,
+  ticksValues: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+});
