e1e30ba0f6c4b90bc3b5124611007bea1754890b
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-06-08 19:06:42 +0300

    Edit options

--- src/range-slider/component/globalOptions.ts
+++ src/range-slider/component/globalOptions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export default interface Options {
     max: number;
     min: number;
@@ -5,40 +6,21 @@ export default interface Options {
     defaultValue: number;
     valueSecond: number;
     isMultiThumb: boolean;
-    showRightProgressBar: boolean;
     showBubble: boolean;
     isVertical: boolean;
     showTicks: boolean;
     ticksValues: number[];
 }
 
-// export interface SliderGlobalOptions {
-//     /**
-//      * Global options of the example plugin.
-//      */
-//     options: Options;
-// }
-    
-// //   /**
-// //    * Function to apply the example plugin to the selected elements of a jQuery result.
-// //    */
-// export interface SliderFunction {
-//     (options: Options): JQuery;
-// }
-    
-// //   /**
-// //    * Declaration of the example plugin.
-// //    */
-// export interface Slider extends SliderGlobalOptions, SliderFunction {
-//     (behavior: 'enable'): JQuery;
-// }
-// //   /**
-// //    * Extend the jQuery result declaration with the example plugin.
-// //    */
-
-// export interface JQuery {
-//     /**
-//      * Extension of the example plugin.
-//      */
-//     slider: Slider;
+// export class DefaultOptions {
+//     max: 100;
+//     min: 0;
+//     step: 1;
+//     defaultValue: 50;
+//     valueSecond: 70;
+//     isMultiThumb: false;
+//     showBubble: false;
+//     isVertical: false;
+//     showTicks: false;
+//     ticksValues: number[];
 // }
