8efbe7824e2da24735b45abb0a248d070482496a
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-06-09 20:00:33 +0300

    Edit thumb tests

--- src/range-slider/View/subView/thumbView/thumbView.test.ts
+++ src/range-slider/View/subView/thumbView/thumbView.test.ts
@@ -6,25 +6,24 @@ describe('test view', () => {
     beforeEach(() => {
       thumb = new ThumbView();
       thumb.init(document.body, true, true, 10, 30);
-      // thumb.createThumb = () => { thumb.createThumb(document.body, false); };
     });
     afterEach(() => {
       document.body.innerHTML = '';
     });
 
     test('write thumb value', () => {
-        thumb.writeBubbleValue(false, 10, undefined);
-        expect(thumb.firstThumb.textContent).toEqual(`First value: ${String(10)}`);
-        thumb.writeBubbleValue(true, 10, 30);
-        expect(thumb.secondThumb.textContent).toEqual(`Second value: ${String(30)}`);
+      thumb.writeBubbleValue(false, 10, undefined);
+      expect(thumb.firstThumb.textContent).toEqual(`First value: ${String(10)}`);
+      thumb.writeBubbleValue(true, 10, 30);
+      expect(thumb.secondThumb.textContent).toEqual(`Second value: ${String(30)}`);
     });
 
     test('place thumb on input', () => {
-        thumb.placeThumb(false, 10, 0);
-        expect(thumb.firstThumb.style.left).toBe(`${10}%`);
-        thumb.placeThumb(true, 10, 30);
-        expect(thumb.secondThumb.style.right).toBe(`${70}%`);
-        thumb.placeThumb(true, 10, 0);
-        expect(thumb.secondThumb.style.right).toBe(`${100}%`);
+      thumb.placeThumb(false, 10, 0);
+      expect(thumb.firstThumb.style.left).toBe(`${10}%`);
+      thumb.placeThumb(true, 10, 30);
+      expect(thumb.secondThumb.style.right).toBe(`${70}%`);
+      thumb.placeThumb(true, 10, 0);
+      expect(thumb.secondThumb.style.right).toBe(`${100}%`);
     });
 });
