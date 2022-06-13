0af8d0bf14a00863c4b04b24491cc36cc516b53c
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-06-13 15:05:04 +0300

    Add presenter tests

--- src/range-slider/Presenter/Presenter.test.ts
+++ src/range-slider/Presenter/Presenter.test.ts
@@ -25,11 +25,10 @@ describe('test Presenter', () => {
       defaultValue: 11,
       valueSecond: 23,
       isMultiThumb: true,
-      showRightProgressBar: false,
       showBubble: true,
       isVertical: true,
       showTicks: true,
-      ticksValues: [0, 50, 100],
+      ticksValues: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
       barColor: '#000000',
       thumbColor: 'green',
       bubbleColor: 'yellow'
@@ -41,72 +40,72 @@ describe('test Presenter', () => {
       bar = new barView();
       ticks = new ticksView();
       thumb = new thumbView();
+      form = new formView();
       view = new View(document.body, ticks, step, bar, thumb, form, data);
 
       presenter = new Presenter(view, model);
-
-      form.init(document.body, true, 0, 100);
     });
-    // test('Model is initialized', () => {
-    //   jest.spyOn(presenter.model, 'init');
-    //   presenter.init();
-    //   expect(presenter.model.init).toHaveBeenCalled();
-    // });
-    // test('View is initialized', () => {
-    //   jest.spyOn(presenter.view, 'init');
-    //   presenter.init();
-    //   expect(presenter.view.init).toHaveBeenCalled();
-    // });
-    // test('presenter subscribed to Model', () => {
-    //   jest.spyOn(presenter.model, 'subscribe');
-    //   presenter.init();
-    //   expect(presenter.model.subscribe).toHaveBeenCalledWith(presenter);
-    // });
-    // test('presenter subscribed to View', () => {
-    //   jest.spyOn(presenter.view, 'subscribe');
-    //   presenter.init();
-    //   expect(presenter.view.subscribe).toHaveBeenCalledWith(presenter);
-    // });
-    test('transfer data from model to view', () => {
-      presenter.model.optionsForView = data;
+
+    test('Model is initialized', () => {
+      jest.spyOn(presenter.model, 'init');
       presenter.init();
+      expect(presenter.model.init).toHaveBeenCalled();
+    });
+
+    test('View is initialized', () => {
+      jest.spyOn(presenter.view, 'init');
+      presenter.init();
+      expect(presenter.view.init).toHaveBeenCalled();
+    });
+
+    test('presenter subscribed to Model', () => {
+      jest.spyOn(presenter.model, 'subscribe');
+      presenter.init();
+      expect(presenter.model.subscribe).toHaveBeenCalledWith(presenter);
+    });
+
+    test('presenter subscribed to View', () => {
+      jest.spyOn(presenter.view, 'subscribe');
+      presenter.init();
+      expect(presenter.view.subscribe).toHaveBeenCalledWith(presenter);
+    });
+
+    test('transfer data from model to view', () => {
+      presenter.view.options = data;
+      presenter.init();
+      presenter.view.init();
       expect(presenter.view.options).toEqual(data);
     });
-    // test('Model updates with correct arguments, default settings', () => {
-    //   const updateArgs = {
-    //     value: 10,
-    //     isDefault: true,
-    //   };
-    //   const { value: correctValue, isDefault: correctOption } = updateArgs;
-    //   jest.spyOn(presenter.model, 'update');
-    //   presenter.updateModel(correctValue, correctOption);
-    //   expect(presenter.model.update).toHaveBeenCalledWith(correctValue, correctOption);
-    // });
-    // test('Model updates with correct arguments, right thumb', () => {
-    //   const updateArgs = {
-    //     value: -1001,
-    //     isDefault: false,
-    //   };
-    //   const { value: correctValue, isDefault: correctOption } = updateArgs;
-    //   jest.spyOn(presenter.model, 'update');
-    //   presenter.updateModel(correctValue, correctOption);
-    //   expect(presenter.model.update).toHaveBeenCalledWith(correctValue, correctOption);
-    // });
-    // test('View updates with correct arguments, default (single or left) thumb', () => {
-    //   const correctThumbValue = 42;
-    //   presenter.model.defaultValue = correctThumbValue;
-    //   presenter.updateView();
-    //   expect(presenter.view.options.defaultValue).toBe(correctThumbValue);
-    // });
-    // test('View updates with correct arguments, right thumb', () => {
-    //   const correctThumbValue = 0;
-    //   presenter.model.valueSecond = correctThumbValue;
-    //   presenter.updateView();
-    //   expect(presenter.view.options.valueSecond).toBe(correctThumbValue);
-    // });
-    // test('setInput function called after View update', () => {
-    //   jest.spyOn(presenter.view, 'setInput');
-    //   presenter.updateView();
-    //   expect(presenter.view.setInput).toHaveBeenCalled();
-    // });
+
+    test('Model updates with correct arguments, default settings', () => {
+      jest.spyOn(presenter.model, 'update');
+      presenter.updateModel(10, true);
+      expect(presenter.model.update).toHaveBeenCalledWith(10, true);
+    });
+
+    test('Model updates with correct arguments, second thumb', () => {
+      jest.spyOn(presenter.model, 'update');
+      presenter.updateModel(1, false);
+      expect(presenter.model.update).toHaveBeenCalledWith(1, false);
+    });
+
+    test('View updates with correct arguments, default thumb', () => {
+      const correctThumbValue = 42;
+      presenter.model.defaultValue = correctThumbValue;
+      presenter.updateView();
+      expect(presenter.view.options.defaultValue).toBe(correctThumbValue);
+    });
+
+    test('View updates with correct arguments, second thumb', () => {
+      const correctThumbValue = 0;
+      presenter.model.valueSecond = correctThumbValue;
+      presenter.updateView();
+      expect(presenter.view.options.valueSecond).toBe(correctThumbValue);
+    });
+
+    test('setInput function called after View update', () => {
+      jest.spyOn(presenter.view, 'setInput');
+      presenter.updateView();
+      expect(presenter.view.setInput).toHaveBeenCalled();
+    });
 });
