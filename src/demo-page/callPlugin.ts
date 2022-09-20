9b5eab0dcb8561fcfa8872d797c2559ff9e6cef3
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-09-20 15:13:59 +0300

    Add fourth slider

--- src/demo-page/callPlugin.ts
+++ src/demo-page/callPlugin.ts
@@ -71,6 +71,29 @@ class CallPlugin {
       bubbleColor: String(panel[11]),
     });
   }
+
+  public renderFourthSlider(id: string, panel: (number | boolean | string)[]) {
+    // render slider with info from panel
+    // delete duplicates of slider when panel is used
+    const elem = <HTMLDivElement>document.getElementById(id);
+    if (elem.firstElementChild) {
+      elem.removeChild(elem.firstElementChild);
+    }
+    $('#api_fourth').sliderPlugin({
+      max: Number(panel[0]),
+      min: Number(panel[1]),
+      step: Number(panel[2]),
+      defaultValue: Number(panel[3]),
+      valueSecond: Number(panel[4]),
+      isMultiThumb: Boolean(panel[5]),
+      showBubble: Boolean(panel[6]),
+      isVertical: Boolean(panel[7]),
+      showTicks: Boolean(panel[8]),
+      barColor: String(panel[9]),
+      thumbColor: String(panel[10]),
+      bubbleColor: String(panel[11]),
+    });
+  }
 }
 const test = new CallPlugin();
 export default test;
--- src/demo-page/panel.ts
+++ src/demo-page/panel.ts
@@ -18,6 +18,7 @@ class Panel {
     this.callFirstApi();
     this.callSecondApi();
     this.callThirdApi();
+    this.callFourthApi();
   }
 
   setState(options: (number | boolean | string)[]): void {
@@ -40,6 +41,11 @@ class Panel {
   callThirdApi() {
     this.getDataFromThirdPanel();
     this.getDataFromThirdState();
+  }
+
+  callFourthApi() {
+    this.getDataFromFourthPanel();
+    this.getDataFromFourthState();
   }
 
   // FIRST PANEL AND API
@@ -403,6 +409,127 @@ class Panel {
     // render slider with data from panel
     test.renderThirdSlider('api_third', this.stateForThirdApi);
     this.spyOnThirdSlider('api_third');
+  }
+
+  // FOURTH PANEL AND API
+  // state gets data from panel
+  getDataFromFourthPanel() {
+    // get values from panel
+    const getValue = (id: string): number => {
+      const elem = <HTMLInputElement>document.getElementById(id);
+      return Number(elem.value);
+    };
+    const getBooleanValue = (id: string): boolean => {
+      const elem = <HTMLInputElement>document.getElementById(id);
+      return Boolean(elem.checked);
+    };
+    const getStringValue = (id: string): string => {
+      const elem = <HTMLInputElement>document.getElementById(id);
+      return String(elem.value);
+    };
+
+    this.setState([
+      getValue('max4'),
+      getValue('min4'),
+      getValue('step4'),
+      getValue('defaultValue4'),
+      getValue('valueSecond4'),
+      getBooleanValue('isMultiThumb4'),
+      getBooleanValue('showBubble4'),
+      getBooleanValue('isVertical4'),
+      getBooleanValue('showTicks4'),
+      getStringValue('barColor4'),
+      getStringValue('thumbColor4'),
+      getStringValue('bubbleColor4'),
+    ]);
+  }
+  
+  // when slider changes default and second values, panel shows changes
+  spyOnFourthSlider(item: string) {
+    const slider = <HTMLDivElement>document.getElementById(item)?.firstChild;
+    const setSliderValue = () => {
+      const newDefaultValue = slider.getAttribute('default-value');
+      if (newDefaultValue) {
+        const element = <HTMLInputElement>document.getElementById('defaultValue4');
+        element.value = newDefaultValue;
+        this.stateForFourthApi[3] = Number(newDefaultValue);
+      }
+      const newDefaultLeftValue = slider.getAttribute('first-value');
+      if (newDefaultLeftValue) {
+        const element = <HTMLInputElement>document.getElementById('defaultValue4');
+        element.value = newDefaultLeftValue;
+        this.stateForFourthApi[3] = Number(newDefaultLeftValue);
+      }
+      const newRightValue = slider.getAttribute('second-value');
+      if (newRightValue) {
+        const element = <HTMLInputElement>document.getElementById('valueSecond4');
+        element.value = newRightValue;
+        this.stateForFourthApi[4] = Number(newRightValue);
+      }
+    };
+    slider.addEventListener('mousemove', setSliderValue);
+    slider.addEventListener('click', setSliderValue);
+  }
+
+  // slider gets data from state
+  getDataFromFourthState() {
+    const maxElement = <HTMLInputElement>document.getElementById('max4');
+    const minElement = <HTMLInputElement>document.getElementById('min4');
+    const stepElement = <HTMLInputElement>document.getElementById('step4');
+    const defaultValueElement = <HTMLInputElement>document.getElementById('defaultValue4');
+    const valueSecondElement = <HTMLInputElement>document.getElementById('valueSecond4');
+    // change number value
+    const changeNumberValue = () => {
+      this.stateForFourthApi[0] = Number(maxElement.value);
+      this.stateForFourthApi[1] = Number(minElement.value);
+      this.stateForFourthApi[2] = Number(stepElement.value);
+      this.stateForFourthApi[3] = Number(defaultValueElement.value);
+      this.stateForFourthApi[4] = Number(valueSecondElement.value);
+      test.renderFourthSlider('api_fourth', this.stateForFourthApi);
+      this.spyOnFourthSlider('api_fourth');
+    };
+    maxElement.addEventListener('change', changeNumberValue);
+    minElement.addEventListener('change', changeNumberValue);
+    stepElement.addEventListener('change', changeNumberValue);
+    defaultValueElement.addEventListener('change', changeNumberValue);
+    valueSecondElement.addEventListener('change', changeNumberValue);
+
+    // change boolean data
+    const multiElement = <HTMLInputElement>document.getElementById('isMultiThumb4');
+    const bubbleElement = <HTMLInputElement>document.getElementById('showBubble4');
+    const isVerticalElement = <HTMLInputElement>document.getElementById('isVertical4');
+    const ticksElement = <HTMLInputElement>document.getElementById('showTicks4');
+    const changeBooleanValue = () => {
+      this.stateForFourthApi[5] = Boolean(multiElement.checked);
+      this.stateForFourthApi[6] = Boolean(bubbleElement.checked);
+      this.stateForFourthApi[7] = Boolean(isVerticalElement.checked);
+      this.stateForFourthApi[8] = Boolean(ticksElement.checked);
+      test.renderFourthSlider('api_fourth', this.stateForFourthApi);
+      this.spyOnFourthSlider('api_fourth');
+    };
+    multiElement.addEventListener('change', changeBooleanValue);
+    bubbleElement.addEventListener('change', changeBooleanValue);
+    isVerticalElement.addEventListener('change', changeBooleanValue);
+    ticksElement.addEventListener('change', changeBooleanValue);
+
+    // change string data
+    const barColorElement = <HTMLInputElement>document.getElementById('barColor4');
+    const thumbColorElement = <HTMLInputElement>document.getElementById('thumbColor4');
+    const bubbleColorElement = <HTMLInputElement>document.getElementById('bubbleColor4');
+    const changeStringValue = () => {
+      this.stateForFourthApi[9] = String(barColorElement.value);
+      this.stateForFourthApi[10] = String(thumbColorElement.value);
+      this.stateForFourthApi[11] = String(bubbleColorElement.value);
+      test.renderFourthSlider('api_fourth', this.stateForFourthApi);
+      this.spyOnFourthSlider('api_fourth');
+    };
+    barColorElement.addEventListener('change', changeStringValue);
+    thumbColorElement.addEventListener('change', changeStringValue);
+    bubbleColorElement.addEventListener('change', changeStringValue);
+    
+    // render slider with data from panel
+    test.renderFourthSlider('api_fourth', this.stateForFourthApi);
+    this.spyOnFourthSlider('api_fourth');
   }
 }
 
